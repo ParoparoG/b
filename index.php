91ee4ae26f4f018326f59ea4a7766da38c29567d
ParoparoG <98857883+ParoparoG@users.noreply.github.com>
2022-02-25 18:13:43 +0800

    Update and rename cle.html to index.php

--- index.php
+++ index.php
@@ -5,14 +5,13 @@
 
 <body background="background.jpg">
 <font color=black>
-<h1 align=center>CLE PT</h1>
+<h1 align=center>CLE</h1>
 </font>
-<hr>
-<center>
-<table border=5 bgcolor=pink width=50%>
-<th>CLE PT</th>
-</table>
-</center>
 <hr>
 </body>
 </html>
+<center>
+<table border=5 bgcolor=pink width=50%>
+<th><a href="Homepage.html">HOMEPAGE</a></th>
+</table>
+</center>
